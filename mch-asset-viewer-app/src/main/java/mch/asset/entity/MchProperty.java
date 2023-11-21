5f424b63923f082dfa46a0710f57137b87acb554
eatrejosm <eatrejosm@gmail.com>
2023-11-21 10:21:17 +0900

    add property entity

--- mch-asset-viewer-app/src/main/java/mch/asset/entity/MchProperty.java
+++ mch-asset-viewer-app/src/main/java/mch/asset/entity/MchProperty.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.Immutable;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -14,5 +15,21 @@ import java.util.List;
 public class MchProperty implements Serializable {
     @Id
     private Long id;
+    private String application;
+    private String label;
+    private String profile;
+
+    @Column(name = "prop_key")
+    private String propKey;
+
+    @Column(name = "prop_value")
+    private String prop_value;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "modified_at")
+    private Date modifiedAt;
+
 
 }
--- mch-asset-viewer-app/src/main/java/mch/asset/entity/UserEntity.java
+++ mch-asset-viewer-app/src/main/java/mch/asset/entity/UserEntity.java
@@ -11,7 +11,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name="users", schema = "mch_user")
+@Table(name="users", schema = "config")
 @Immutable
 @Builder
 @AllArgsConstructor
